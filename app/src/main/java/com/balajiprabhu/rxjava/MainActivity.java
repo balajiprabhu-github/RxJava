965f75837aef838ae9668ae809f67904de8fa512
balajiprabhu-github <balajiprabhu008@gmail.com>
2020-01-25 23:09:15 +0530

    Observable to Flowable

--- app/src/main/java/com/balajiprabhu/rxjava/MainActivity.java
+++ app/src/main/java/com/balajiprabhu/rxjava/MainActivity.java
@@ -4,22 +4,59 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subscribers.ResourceSubscriber;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "MainActivityLog";
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Observable<Integer> integerObservable = PublishSubject.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                for (int i = 0; i <= 1000; i++) {
+                    emitter.onNext(i);
+                }
+
+                emitter.onComplete();
+            }
+        });
+
+        integerObservable.toFlowable(BackpressureStrategy.BUFFER).observeOn(Schedulers.io()).subscribe(new ResourceSubscriber<Integer>() {
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "onNext: " + integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Log.d(TAG, "onError: " + t.getLocalizedMessage());
+
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        });
+
+    }
+
+    public void observableAndObservesrs() {
         Observable<Task> taskObservable = Observable
                 .fromIterable(DataSource.createTasksList())
                 .filter(new Predicate<Task>() {
@@ -59,10 +96,8 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onComplete: ");
             }
         });
-
-
     }
 
-
-
 }
+
+
